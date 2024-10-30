d811b62854546ed1e014791df14d1a6ed3f0a64d
ryan mottram <ryan.mottram@stu.ocsb.ca>
2024-10-30 11:15:02 -0400

    lesson 1

--- main.js
+++ main.js
@@ -27,3 +27,38 @@ function round(value, decimals) {
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function user() {
+    let age = parseInt(prompt("Please enter your age:"));
+
+    if (age >= 60) {
+        console.log("You qualify for the senior discount!");
+        return
+    }
+    if (age < 16) {
+        console.log("You're not old enough to drive yet.");
+    }
+    if (age === 44) {
+        console.log("So is Mr. Squirrel!");
+    }
+
+    let user_name = prompt("Please enter your name:");
+
+    if (user_name === "Mr. Squirrel") {
+        console.log("🐿️");
+    }
+    if (user_name.length > 7) {
+        console.log("You have a long name.");
+    }
+
+    let name_length_guess = parseInt(prompt("How long do you think your name is (in characters)?"));
+
+    if (name_length_guess === user_name.length) {
+        console.log("That's correct! ✔️");
+    } else if (name_length_guess > user_name.length) {
+        console.log("Too high ✖️");
+    } else {
+        console.log("Too low ✖️");
+    }
+}
+
